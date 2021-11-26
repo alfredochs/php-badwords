7f4c3b99b813f1d94e3fed908aa1c2f49ba0c766
alfredochs <chanta.alfredo@gmail.com>
2021-11-26 14:11:07 +0100

    prova commit

---
+++ intro.php
@@ -0,0 +1,18 @@
+<?php
+$name = "alfredo";
+$surname="Chanta";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>intro PHP</title>
+</head>
+<body>
+    <h1>benvenuto</h1>
+    
+</body>
+</html>
